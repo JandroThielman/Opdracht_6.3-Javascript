a9867c1435d8b21bb67af5964a908612f1fe2f28
JandroThielman <9020498@student.zadkine.nl>
2023-10-11 13:54:07 +0200

    Magic

--- JavaScript.js
+++ JavaScript.js
@@ -3,4 +3,6 @@ var x = document.getElementById("effect");
 function Magic(){
     x.play();
     document.getElementById("visible").style.visibility = "visible";
+    document.body.style.backgroundColor = "rgb(39, 7, 39)";
+    document.getElementById("magic").style.transform = "rotate(360)";
 }
